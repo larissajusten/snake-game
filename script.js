6131c9f5177111499ca5a39a12f0b83c7478ee8e
Larissa Espelocin <larissa.justen@cwi.com.br>
2020-05-01 05:48:06 -0300

    Adicionando layout

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d"); //tratar o arquivo como plano 2d
+let context = canvas.getContext("2d");
+
 const velocidadeInicial = 150;
 
 let box = 32;
@@ -15,6 +16,7 @@ cobrinha[0] = {
 function geraNumeroAleatorio() {
   return Math.floor(Math.random() * 15 + 1) * box;
 }
+
 let frutinha = {
   x: geraNumeroAleatorio(),
   y: geraNumeroAleatorio()
@@ -22,23 +24,19 @@ let frutinha = {
 
 function mantemLoopDaCobrinha() {
   if (cobrinha[0].x > 15 * box && direcao == "right") {
-    clearInterval(jogo);
-    alert("Game over!");
+    cobrinha[0].x = 0;
   }
 
   if (cobrinha[0].x < 0 && direcao == "left") {
-    clearInterval(jogo);
-    alert("Game over!");
+    cobrinha[0].x = 16 * box;
   }
 
   if (cobrinha[0].y < 0 && direcao == "up") {
-    clearInterval(jogo);
-    alert("Game over!");
+    cobrinha[0].y = 16 * box;
   }
 
   if (cobrinha[0].y > 15 * box && direcao == "down") {
-    clearInterval(jogo);
-    alert("Game over!");
+    cobrinha[0].y = 0;
   }
 }
 
@@ -53,19 +51,21 @@ function verificaSeCobrinhaSeChoca() {
 }
 
 function criarBG() {
-  context.fillStyle = "lightgreen"; //estilo do canvas/contexto
-  context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retangulo (pos x, pos y, largura, altura)
+  context.fillStyle = "#fffffe";
+  context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
   for (i = 0; i < cobrinha.length; i++) {
-    context.fillStyle = "green";
+    context.fillStyle = "#ff8906";
     context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
   }
 }
 
 function desenharFrutinha() {
-  context.fillStyle = "red";
+  context.fillStyle = "#f25f4c";
+  context.lineWidth = 2;
+  context.strokeStyle = "#FF0000";
   context.fillRect(frutinha.x, frutinha.y, box, box);
 }
 
@@ -96,7 +96,6 @@ function comportamentoDaCobrinha() {
 
 document.addEventListener('keydown', atualizarEventoSetas);
 function atualizarEventoSetas(event) {
-  //KeyCode das setas é na direção horária
   if (event.keyCode == 37 && direcao !== "right") direcao = "left";
   if (event.keyCode == 38 && direcao !== "down") direcao = "up";
   if (event.keyCode == 39 && direcao !== "left") direcao = "right";
@@ -113,3 +112,99 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, velocidadeAtual);
+
+
+//Backgroud 
+//Fonte: https://codepen.io/pirrera/pen/kDsJa?__cf_chl_captcha_tk__=e0e240785baf55a752657b24e1d55e3682f0ec32-1588322806-0-AXcFn2WD-p_uKHaOwNVmp9j2A-mlEd4invOcyfXZ7LhHLKHfZ1eF1mp6eCCIj5AScRq1NtVfVzM8LMtmjmiU9vQTz3QQBB20yk4TdKnteZwraWuf1j_QqxcvEKf0xTCxAwFI8sMwYcdkVPLqdRp6xCt3Cm1zQAnZLCsXmMRl7w3wLgwrFEkduso6KlX7N2JEiFRylwpfpPl_6J3XWEWZ3uKHfn-7BejTT-KVg_1OGtojwIZkGDx5D4dO3_4c9EmgGRnltB_p512k0hA1EqCKYEnkP6tUW3wf32vSr35j_y3ET8Y3IY7-4eGUWMWAbV_8YN7EPGws6TpD22NJwNa2GLa9xOvXMT10UmA4TeUofzlQ3Tv-vC9T_hbtYFITVW9m39lBuqoeZIIgjAiHala9pyk6yw-nC7W78TyCg8SUMJkcXdVdpocHnQPiUYt5hWWfuDFTqDiw2r_jvR9mz_9y8-1OP0WUKOeToD6Cg3Rc7LwNOl74Hl105DMfmXnT6LE5Iy2nANDCJLDAPhl3-TpdX4Xt3L5JOo8y4DXrsFjGBgFOSNfB-fpJKkupuOljZP1P6Q
+(function () {
+
+  var width, height, largeContainer, canvas, ctx, circles, target, animateHeader = true;
+
+  // Main
+  initHeader();
+  addListeners();
+
+  function initHeader() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    target = { x: 0, y: height };
+
+    largeContainer = document.getElementById('co');
+    largeContainer.style.height = height + 'px';
+
+    canvas = document.getElementById('c');
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext('2d');
+
+    // create particles
+    circles = [];
+    for (var x = 0; x < width * 0.5; x++) {
+      var c = new Square();
+      circles.push(c);
+    }
+    animate();
+  }
+
+  // Event handling
+  function addListeners() {
+    window.addEventListener('scroll', scrollCheck);
+    window.addEventListener('resize', resize);
+  }
+
+  function scrollCheck() {
+    if (document.body.scrollTop > height) animateHeader = false;
+    else animateHeader = true;
+  }
+
+  function resize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    largeContainer.style.height = height + 'px';
+    canvas.width = width;
+    canvas.height = height;
+  }
+
+  function animate() {
+    if (animateHeader) {
+      ctx.clearRect(0, 0, width, height);
+      for (var i in circles) {
+        circles[i].draw();
+      }
+    }
+    requestAnimationFrame(animate);
+  }
+
+  // Canvas manipulation
+  function Square() {
+    var _this = this;
+
+    // constructor
+    (function () {
+      _this.pos = {};
+      init();
+      console.log(_this);
+    })();
+
+    function init() {
+      _this.pos.x = Math.random() * width;
+      _this.pos.y = height + Math.random() * 100;
+      _this.alpha = 0.1 + Math.random() * 0.3;
+      _this.scale = 0.1 + Math.random() * 0.3;
+      _this.velocity = Math.random();
+    }
+
+    this.draw = function () {
+      if (_this.alpha <= 0) {
+        init();
+      }
+      _this.pos.y -= _this.velocity;
+      _this.alpha -= 0.0005;
+      ctx.beginPath();
+      ctx.arc(_this.pos.x, _this.pos.y, 1, 0, 3, false);
+      ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
+      ctx.fill();
+    };
+  }
+
+})();
